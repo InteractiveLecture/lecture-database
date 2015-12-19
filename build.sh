6a5a0628a3d23381cbbb9140d76da67876d26146
Rene Richter <Richterrettich@gmail.com>
2015-12-19 10:24:15 +0100

    Move build logic to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
+set -ev
 docker build -t openservice/lecture-database:latest .
+cd testdb && docker build -t openservice/lecture-testdatabase:latest .
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/lecture-database" ] ; then
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL"
+  docker push openservice/lecture-database:latest
+  docker push openservice/lecture-testdatabase:latest
+fi
